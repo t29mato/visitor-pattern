b14764eb27da0ee8bb2b6ae167a787fc9de19475
t.mato <t.mato@i3design.co.jp>
2019-09-01 23:24:22 +0900

    src init for changing another example

--- src/Element.ts
+++
@@ -1,5 +0,0 @@
-import Visitor from './Visitor';
-
-export default interface Element {
-    accept(visitor: Visitor): void;
-}
--- src/Visitor.ts
+++
@@ -1,6 +0,0 @@
-import Directory from './Directory';
-
-export default abstract class Visitor {
-    abstract visit(file: File): void;
-    abstract visit(directory: Directory): void;
-}
